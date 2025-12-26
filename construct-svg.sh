926443f2adab2da46af294037bdb106a3f843a98
anthony1x6000 <33004321+anthony1x6000@users.noreply.github.com>
2025-12-25 20:04:28 -0500

    update

---
+++ construct-svg.sh
@@ -0,0 +1,10 @@
+echo '<svg fill="none" xmlns="http://www.w3.org/2000/svg">
+    <foreignObject width="100%" height="100%">
+        <div xmlns="http://www.w3.org/1999/xhtml">' > embed.svg
+
+cat index.html >> embed.svg
+
+echo '
+        </div>
+    </foreignObject>
+</svg>' >> embed.svg
